aa20b513b3efb1ce2ee1fc2e7916319152681784
Loan Vaque <loanvaque@users.noreply.github.com>
2018-11-22 09:02:56 +0100

    Initial commit

---
+++ weatherForecast.go
@@ -0,0 +1,209 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"bytes"
+	"net/url"
+)
+
+type DatasetConfig struct {
+	FileName string
+	Title string `json:"title"`
+	Openweathermap struct {
+		ApiUrl string `json:"apiUrl"`
+		ApiKey string `json:"apiKey"`
+		CityId string `json:"cityId"`
+	} `json:"openweathermap"`
+	Highcharts struct {
+		ApiUrl string `json:"apiUrl"`
+	} `json:"highcharts"`
+}
+
+type OpenweathermapJsonIn struct {
+	List []struct {
+		Date string `json:"dt_txt"`
+		Main struct {
+			Temp float64 `json:"temp"`
+		} `json:"main"`
+		Cloud struct {
+			Cover float64 `json:"all"`
+		} `json:"clouds"`
+		Wind struct {
+			Speed float64 `json:"speed"`
+		} `json:"wind"`
+		Rain struct {
+			Precip float64 `json:"3h"`
+		} `json:"rain"`
+	} `json:"list"`
+	City struct {
+		Name string `json:"name"`
+	} `json:"city"`
+}
+
+type HighchartsJsonOut struct {
+	Chart struct {
+		Type string `json:"type"`
+	} `json:"chart"`
+	Title struct {
+		Text string `json:"text"`
+	} `json:"title"`
+	Subtitle struct {
+		Text string `json:"text"`
+	} `json:"subtitle"`
+	Yaxis []HighchartsYaxis `json:"yAxis"`
+	Xaxis struct {
+		Categories []string `json:"categories"`
+	} `json:"xAxis"`
+	PlotOptions struct {
+		Spline struct {
+			DataLabels struct {
+				Enabled bool `json:"enabled"`
+			} `json:"dataLabels"`
+		} `json:"spline"`
+		Series struct {
+			Marker struct {
+				Symbol string `json:"symbol"`
+			} `json:"marker"`
+		} `json:"series"`
+	} `json:"plotOptions"`
+	Series []HighchartsSerie `json:"series"`
+}
+
+type HighchartsYaxis struct {
+	Min int `json:"min"`
+	Max int `json:"max"`
+	Visible bool `json:"visible"`
+}
+
+type HighchartsSerie struct {
+	Name string `json:"name"`
+	Yaxis int `json:"yAxis"`
+	Color string `json:"color"`
+	DataLabels HighchartsLabels `json:"dataLabels"`
+	Data []int64 `json:"data"`
+}
+
+type HighchartsLabels struct {
+	Format string `json:"format"`
+	Style struct {
+		Color string `json:"color"`
+	} `json:"style"`
+}
+
+func main() () {
+	// construct default config
+	config := &DatasetConfig{}
+	config.FileName = "weatherForecast.json"
+	// load config from file
+	byteString, err := ioutil.ReadFile(config.FileName)
+	if err != nil {
+		fmt.Printf("unable to read config file > %v\n", err)
+		return
+	}
+	if !json.Valid(byteString) {
+		fmt.Printf("config file content is not json compliant\n")
+		return
+	}
+	err = json.Unmarshal(byteString, config)
+	if err != nil {
+		fmt.Printf("unable to unmarshal json content of config file > %v\n", err)
+		return
+	}
+	// query openweathermap api
+	httpResponse, err := http.Get(config.Openweathermap.ApiUrl + "?APPID=" + config.Openweathermap.ApiKey + "&id=" + config.Openweathermap.CityId + "&units=metric")
+	if err != nil {
+		fmt.Printf("unable to query openweathermap api > %v\n", err)
+		return
+	}
+	defer httpResponse.Body.Close()
+	// parse to json
+	jsonBytesIn, err := ioutil.ReadAll(httpResponse.Body)
+	if err != nil {
+		fmt.Printf("unable to read response content > %v\n", err)
+		return
+	}
+	jsonIn := OpenweathermapJsonIn{}
+	err = json.Unmarshal(jsonBytesIn, &jsonIn)
+	if err != nil {
+		fmt.Printf("unable to unmarshal json payload > %v\n", err)
+		return
+	}
+	// convert to highcharts
+	highchartsJsonOut := &HighchartsJsonOut{}
+	highchartsJsonOut.Chart.Type = "spline"
+	highchartsJsonOut.Title.Text = config.Title
+	highchartsJsonOut.Subtitle.Text = jsonIn.City.Name + ", " + jsonIn.List[0].Date[0:10]
+	highchartsJsonOut.PlotOptions.Spline.DataLabels.Enabled = true
+	highchartsJsonOut.PlotOptions.Series.Marker.Symbol = "circle"
+
+	yaxis0 := HighchartsYaxis{}
+	yaxis0.Min = 0
+	yaxis0.Max = 30
+	yaxis0.Visible = false
+	highchartsJsonOut.Yaxis = append(highchartsJsonOut.Yaxis, yaxis0)
+
+	yaxis1 := HighchartsYaxis{}
+	yaxis1.Min = 0
+	yaxis1.Max = 90
+	yaxis1.Visible = false
+	highchartsJsonOut.Yaxis = append(highchartsJsonOut.Yaxis, yaxis1)
+
+	yaxis2 := HighchartsYaxis{}
+	yaxis2.Min = 0
+	yaxis2.Max = 20
+	yaxis2.Visible = false
+	highchartsJsonOut.Yaxis = append(highchartsJsonOut.Yaxis, yaxis2)
+
+	yaxis3 := HighchartsYaxis{}
+	yaxis3.Min = 0
+	yaxis3.Max = 100
+	yaxis3.Visible = false
+	highchartsJsonOut.Yaxis = append(highchartsJsonOut.Yaxis, yaxis3)
+
+	serie0 := HighchartsSerie{"Temperature", 0, "#AB3C3C", HighchartsLabels{}, []int64{}}
+	serie0.DataLabels.Style.Color = "#AB3C3C"
+	serie0.DataLabels.Format = "{y}°C"
+	serie1 := HighchartsSerie{"Rainfall", 1, "#308930", HighchartsLabels{}, []int64{}}
+	serie1.DataLabels.Style.Color = "#308930"
+	serie1.DataLabels.Format = "{y}mm"
+	serie2 := HighchartsSerie{"Wind speed", 2, "#AB953C", HighchartsLabels{}, []int64{}}
+	serie2.DataLabels.Style.Color = "#AB953C"
+	serie2.DataLabels.Format = "{y}m/s"
+	serie3 := HighchartsSerie{"Cloud cover", 3, "#3F3376", HighchartsLabels{}, []int64{}}
+	serie3.DataLabels.Style.Color = "#3F3376"
+	serie3.DataLabels.Format = "{y}%"
+	for index := 0; index < 8; index++ {
+		highchartsJsonOut.Xaxis.Categories = append(highchartsJsonOut.Xaxis.Categories, jsonIn.List[index].Date[11:16])
+		serie0.Data = append(serie0.Data, int64(jsonIn.List[index].Main.Temp))
+		serie1.Data = append(serie1.Data, int64(jsonIn.List[index].Rain.Precip))
+		serie2.Data = append(serie2.Data, int64(jsonIn.List[index].Wind.Speed))
+		serie3.Data = append(serie3.Data, int64(jsonIn.List[index].Cloud.Cover))
+	}
+	highchartsJsonOut.Series = append(highchartsJsonOut.Series, serie0)
+	highchartsJsonOut.Series = append(highchartsJsonOut.Series, serie1)
+	highchartsJsonOut.Series = append(highchartsJsonOut.Series, serie2)
+	highchartsJsonOut.Series = append(highchartsJsonOut.Series, serie3)
+	// prepare payload
+	bytesOut, err := json.Marshal(&highchartsJsonOut)
+	if err != nil {
+		fmt.Printf("unable to marshal json payload > %v\n", err)
+		return
+	}
+	// query highcharts
+//	fmt.Printf("%v\n", string(bytesOut))
+	stringEscaped := "async=true&type=png&options=" + url.QueryEscape(string(bytesOut))
+	httpResponse, err = http.Post(config.Highcharts.ApiUrl, "application/x-www-form-urlencoded", bytes.NewReader([]byte(stringEscaped)))
+	if err != nil {
+		fmt.Printf("unable to query highcharts api > %v\n", err)
+		return
+	}
+	defer httpResponse.Body.Close()
+	// dump response
+	bytesIn, err := ioutil.ReadAll(httpResponse.Body)
+	fmt.Printf("%v%v\n", config.Highcharts.ApiUrl, string(bytesIn))
+
+	return
+}
